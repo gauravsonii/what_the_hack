8e2178d27aaad8682332db58a29f023b05a24a15
GAURAV SONI <149526482+gauravsonii@users.noreply.github.com>
2025-08-27 22:57:43 +0530

    Update nav.tsx

--- components/nav.tsx
+++ components/nav.tsx
@@ -6,7 +6,7 @@ const Nav = () => {
   return (
     <nav className="flex ml-auto mr-4 pt-4 md:mr-10 md:pt-10">
       <Link
-        href="https://github.com/lakshaybhushan/what-the-hack"
+        href="https://github.com/gauravsonii/what-the-hack"
         rel="noopener noreferrer"
         target="_blank"
         className="font-medium transition duration-300 ease-linear flex items-center gap-3 bg-primary/10 px-6 py-4 rounded-full hover:bg-primary/30 hover:text-primary text-primary/80">
